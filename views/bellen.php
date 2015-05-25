94afe3795debb640005920e5947887aa88cc4ff4
Max Windau <Max.Windau@gmail.com>
2015-05-25 19:05:35 +0200

    Added calling layout

---
+++ views/bellen.php
@@ -0,0 +1,11 @@
+<div class="col-sm-12">
+    <h3>Belronde</h3>
+    <h1><i class="fa fa-phone"></i> 06-12727301</h1>
+</div>
+
+<div class="col-sm-4">
+    
+</div>
+<div class="col-sm-8">
+    
+</div>
