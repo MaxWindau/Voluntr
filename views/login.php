53af8ddd82ab98d75d2d212136e7602361b3f7de
Max Windau <Max.Windau@gmail.com>
2015-07-14 20:17:08 +0200

    Layed-out login-page

--- views/login.php
+++ views/login.php
@@ -1,19 +1,15 @@
-<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-    <h2>Inloggen bij Voluntr</h2>
-    <div class="form-group">
-        <a class="btn btn-sm btn-social btn-facebook"><i class="fa fa-facebook"></i>Inloggen met Facebook</a>
-        <a class="btn btn-sm btn-social btn-twitter"><i class="fa fa-twitter"></i>Inloggen met Twitter</a> 
-        <a class="btn btn-sm btn-social btn-google-plus"><i class="fa fa-google-plus"></i>Inloggen met Google+</a>
-    </div>
-    <h4>Voluntr-account</h4>
+<div class="login-dialog col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+    <h1 class="no-margin login-title"><i class="fa fa-puzzle-piece"></i> </h1>
+    <h3 style="text-align:center;" class="no-margin">Inloggen bij Voluntr<br /><br /></h3>
     <form>
         <div class="form-group">
-            <input type="email" class="form-control input-lg" id="email" placeholder="E-mailadres">
+            <input type="email" class="form-control input-lg" id="email" placeholder="E-mailadres" autofocus>
         </div>
         <div class="form-group">
             <input type="password" class="form-control input-lg" id="wachtwoord" placeholder="Wachtwoord">
         </div>
 
-        <button type="submit" class="btn btn-lg btn-primary pull-right">Aan de slag! <i class="fa fa-arrow-right"></i></button>
+        <button type="submit" class="btn btn-lg btn-header pull-right">Inloggen <i class="fa fa-arrow-right"></i>
+        </button>
     </form>
 </div>
